bd94899021925df5b7f5cf7724377a58af9e47de
Pedro Gonzalez <pedro0gamer0ghht@gmail.com>
2026-03-18 15:59:39 +0100

    Añadiendo el archivo .py y el readme

---
+++ PGH.py
@@ -0,0 +1,14 @@
+import random
+num1 = random.randint(1, 10)
+num2 = random.randint(1, 10)
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print(f"La suma de {num1} y {num2} es: {suma}")
+print(f"La resta de {num1} y {num2} es: {resta}")
+print(f"La multiplicación de {num1} y {num2} es: {multiplicacion}")
+print(f"La division de {num1} y {num2} es: {division}")
+
